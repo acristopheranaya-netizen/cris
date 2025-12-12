f622e73324e4c7aaf04b764d560da7784c4152e8
acristopheranaya-netizen <a.cristopheranaya@seoane.edu.pe>
2025-12-11 20:10:44 -0500

    Add files via upload

---
+++ nosotros.js
@@ -0,0 +1,8 @@
+<script>
+  const questions = document.querySelectorAll(".faq-question");
+  questions.forEach(btn = {
+    btn.addEventListener("click", () => {
+      const answer = btn.nextElementSibling;
+      answer.style.display = answer.style.display === "block" ? "none" : "block";})
+  });
+</script>
